e5469a6136c0d06bb335a368fa7c4ae94e9ff9fa
Paul Barbenheim <paul@barbenheim.de>
2026-01-16 22:37:49 +0100

    added asyncresult method

--- lib/src/types/result_optional.dart
+++ lib/src/types/result_optional.dart
@@ -2,11 +2,22 @@ import 'optional.dart';
 import 'result.dart';
 
 extension ResultOptionalMethods<S extends Object> on Result<Optional<S>> {
-  ResultDart<Optional<W>, Exception> mapOptional<W extends Object>(
+  Result<Optional<W>> mapOptional<W extends Object>(
     W Function(S value) transform,
   ) {
     return map((optional) {
       return optional.map(transform);
+    });
+  }
+}
+
+extension AsyncResultOptionalMethods<S extends Object>
+    on AsyncResult<Optional<S>> {
+  AsyncResult<Optional<W>> mapOptional<W extends Object>(
+    W Function(S value) transform,
+  ) {
+    return then((result) {
+      return result.mapOptional(transform);
     });
   }
 }
